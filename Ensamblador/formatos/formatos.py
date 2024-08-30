2480e75d6508a88480da2580e850794aee6b0db3
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2024-08-29 21:22:01 -0500

    Implementación alias
    Implementación de los alias de los registros

--- Ensamblador/formatos/formatos.py
+++ Ensamblador/formatos/formatos.py
@@ -4,7 +4,7 @@ from .R import obtener_funct3_tipo_r, obtener_funct7_tipo_r
 from .I import obtener_opcode_tipo_i, obtener_funct3_tipo_i
 from .S import obtener_funct3_tipo_s
 from .B import obtener_funct3_tipo_b
-from utilidades import labels, distancia_label, numero_a_binario
+from utilidades import labels, distancia_label, numero_a_binario, registros
 
 def codificar_tipo_r(partes):
     opcode = "0110011"  # opcode para las instrucciones tipo R
@@ -15,9 +15,9 @@ def codificar_tipo_r(partes):
     #partes [1] es el rd, [2] es el rs1, [3] es el rs2
     #[1:] para quitar el x
     #format(int, '05b') para convertir el entero a binario de 5 bits
-    rd = format(int(partes[1][1:]), '05b')   
-    rs1 = format(int(partes[2][1:]), '05b') 
-    rs2 = format(int(partes[3][1:]), '05b')  
+    rd = registros(partes[1])   
+    rs1 = registros(partes[2])
+    rs2 = registros(partes[3])  
     
     return f"{funct7} {rs2} {rs1} {funct3} {rd} {opcode}"
 
@@ -44,8 +44,8 @@ def codificar_tipo_i(partes):
         sys.exit("Inmediato fuera de rango para tipo I")
     
     
-    rd = format(int(partes[1][1:]), '05b') # Quitamos la x y convertimos el rd a binario
-    rs1_bin = format(int(rs1[1:]), '05b') # Quitamos la x y convertimos el rs1 a binario
+    rd = registros(partes[1])  # Quitamos la x y convertimos el rd a binario
+    rs1_bin = registros(rs1) # Quitamos la x y convertimos el rs1 a binario
     # imm_bin = format(int(inmediato), '012b')  
     imm_bin = numero_a_binario(inmediato, 12) # Convierte el inmediato a 12 bits
     
@@ -67,9 +67,9 @@ def codificar_tipo_s(partes):
     if( int(inmediato) < -2048 or int(inmediato) > 2047):
         sys.exit("Inmediato fuera de rango para tipo S")
     
-    rs1 = format(int(rs1[1:]), '05b') # Quitamos la x y convertimos el rs1 a binario
-    rs2 = format(int(rs2[1:]), '05b') # Quitamos la x y convertimos el rs2 a binario
-    imm = format(int(inmediato), '012b') # Convierte el inmediato a 12 bits
+    rs1 = registros(rs1) # Quitamos la x y convertimos el rs1 a binario
+    rs2 = registros(rs2) # Quitamos la x y convertimos el rs2 a binario
+    imm = numero_a_binario(inmediato) # Convierte el inmediato a 12 bits
     
     return f"{imm[0:7]} {rs2} {rs1} {funct3} {imm[7:12]} {opcode}"
     
@@ -78,8 +78,8 @@ def codificar_tipo_b(partes, i):
     opcode = "1100011"  # opcode para las instrucciones tipo B
     funct3 = obtener_funct3_tipo_b(partes[0])
     
-    rs1 = format(int(partes[1][1:]), '05b') # Quitamos la x y convertimos el rs1 a binario
-    rs2 = format(int(partes[2][1:]), '05b') # Quitamos la x y convertimos el rs2 a binario
+    rs1 = registros(partes[1]) # Quitamos la x y convertimos el rs1 a binario
+    rs2 = registros(partes[2]) # Quitamos la x y convertimos el rs2 a binario
     
     
     inmediato = partes[3]
@@ -101,7 +101,7 @@ def codificar_tipo_b(partes, i):
 
 def codificar_tipo_u(partes):
     opcode = "0110111"  # opcode para las instrucciones tipo U
-    rd = format(int(partes[1][1:]), '05b') # Quitamos la x y convertimos el rd a binario
+    rd = registros(partes[1]) # Quitamos la x y convertimos el rd a binario
     inmediato = format(int(partes[2]), '032b')  # Convierte el inmediato a 20 bits
     
     
@@ -109,7 +109,7 @@ def codificar_tipo_u(partes):
     
 def codificar_tipo_j(partes, i):
     opcode = "1101111"
-    rd = format(int(partes[1][1:]), '05b')
+    rd = registros(partes[1])
     inmediato = partes[2]
     linea_label = labels.get(inmediato)
     distancia = distancia_label(linea_label, i)
--- Ensamblador/utilidades.py
+++ Ensamblador/utilidades.py
@@ -1,3 +1,40 @@
+
+equivalencias = {
+    "zero": "x0",
+    "ra": "x1",
+    "sp": "x2",
+    "gp": "x3",
+    "tp": "x4",
+    "t0": "x5",
+    "t1": "x6",
+    "t2": "x7",
+    "s0": "x8",
+    "fp": "x8",
+    "s1": "x9",
+    "a0": "x10",
+    "a1": "x11",
+    "a2": "x12",
+    "a3": "x13",
+    "a4": "x14",
+    "a5": "x15",
+    "a6": "x16",
+    "a7": "x17",
+    "s2": "x18",
+    "s3": "x19",
+    "s4": "x20",
+    "s5": "x21",
+    "s6": "x22",
+    "s7": "x23",
+    "s8": "x24",
+    "s9": "x25",
+    "s10": "x26",
+    "s11": "x27",
+    "t3": "x28",
+    "t4": "x29",
+    "t5": "x30",
+    "t6": "x31",
+}
+
 def leer_instrucciones(archivo):
     with open(archivo, 'r') as archivo:
         instrucciones = archivo.readlines() #Lee todas las lineas del archivo y las guarda en una lista
@@ -23,3 +60,12 @@ def numero_a_binario(number: int | str, length=4):
     binary = int(number).to_bytes(length=4, signed=True)
     normal_binary = ''.join(format(byte, '08b') for byte in binary)
     return normal_binary[-length:]
+
+def registros(reg: str):
+    if not "x" in reg:
+        x_reg = equivalencias.get(reg)
+        if not x_reg:
+            raise ValueError(f"Registro Invalido: '{reg}'")
+        reg = x_reg
+    num_reg = int(reg[1:])
+    return numero_a_binario(num_reg, 5)
